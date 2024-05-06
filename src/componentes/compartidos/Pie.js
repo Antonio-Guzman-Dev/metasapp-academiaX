18426a499b36d945249a75487e04d48aacb08ab6
Josué Antonio Contreras Guzmán <antonioguzman_dev@hotmail.com>
2024-05-05 20:09:57 -0600

    Instalacion de tailwind

---
+++ src/componentes/compartidos/Pie.js
@@ -0,0 +1,9 @@
+function Pie() {
+    return (
+      <footer className="text-red-500">
+        <small>Desarrollado y diseñado por Xavier Reyes Ochoa ©2024. Todos los derechos reservados</small>
+      </footer>
+    );
+  }
+  
+  export default Pie;
---
+++ src/componentes/compartidos/Principal.js
@@ -0,0 +1,15 @@
+function Principal({children}) {
+    return (
+      <>
+        <sidenav>
+          <a href="/lista">Lista</a>
+          <a href="/crear">Crear</a>
+        </sidenav>
+        <div>
+          {children}
+        </div>
+      </>
+    );
+  }
+  
+  export default Principal;
